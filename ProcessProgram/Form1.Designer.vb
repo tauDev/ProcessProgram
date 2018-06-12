98e98f0635c7141e308f8cc64ca170f0c2649d63
Parinya <parinyat0926@gmail.com>
2018-06-12 15:27:45 +0700

    Initial commit

---
+++ ProcessProgram/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ExportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 27)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1214, 586)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 401)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(171, 212)
+        Me.ListBox1.TabIndex = 1
+        Me.ListBox1.Visible = False
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(1179, 530)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(89, 83)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        Me.ListView1.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExportToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1228, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ExportToolStripMenuItem
+        '
+        Me.ExportToolStripMenuItem.Name = "ExportToolStripMenuItem"
+        Me.ExportToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.ExportToolStripMenuItem.Text = "Export"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1228, 619)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ExportToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ProcessProgram/Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+Imports System.Diagnostics
+Imports System.ComponentModel
+Imports Microsoft.Win32
+Imports System.Text
+Imports System.Management
+Imports Microsoft.Office
+
+Public Class Form1
+    Dim dt As New DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dt.Columns.Add("Program")
+        'GetInstallApplications()
+        'Dim p() As Process
+        'p = Process.GetProcesses()
+        'For Each Pro As AppInfo In GetInstalledApps()
+        '    If Pro.UnInstallPath IsNot Nothing Then
+        '        Dim LI As New ListViewItem
+        '        LI.SubItems.Add(Pro.UnInstallPath.Replace("""", ""))
+        '        LI.SubItems.Add(Pro.SilentUnInstallPath)
+        '        ListView1.Items.Add(LI.Name)
+        '    End If
+        ' ListBox1.Items.AddRange(New Object() {Pro.Id, Pro.ProcessName})
+        ' ListBox1.Items.Add(Pro.ProcessName & " # " & Pro.MainWindowTitle & " # " & Pro.Id)
+        'Next
+        For Each AI As AppInfo In GetInstalledApps()
+            If AI.UnInstallPath IsNot Nothing Then
+                Dim LI As New ListViewItem
+                LI.Text = AI.Name
+                LI.SubItems.Add(AI.UnInstallPath.Replace("""", ""))
+                LI.SubItems.Add(AI.SilentUnInstallPath)
+                ListView1.Items.Add(LI)
+                ' dt.Rows.Add(LI)
+            End If
+        Next
+        For i As Integer = 0 To ListView1.Items.Count - 1
+            dt.Rows.Add(ListView1.Items(i).SubItems(0).Text)
+        Next
+        DataGridView1.DataSource = dt
+    End Sub
+    'Private Function GetInstallApplications() As String
+    '    Dim key As RegistryKey
+    '    Dim st As New StringBuilder
+    '    key = Registry.LocalMachine.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Uninstall\")
+    '    For Each subkey As String In key.GetSubKeyNames
+    '        st.AppendLine(subkey)
+    '        dt.Rows.Add(subkey)
+    '    Next
+    '    Return st.ToString
+    'End Function
+    Structure AppInfo
+        Dim Name As String
+        Dim UnInstallPath As String
+        Dim SilentUnInstallPath As String
+    End Structure
+    Private Function GetInstalledApps() As List(Of AppInfo)
+        Dim DestKey As String = "Software\Microsoft\Windows\CurrentVersion\Uninstall\"
+        Dim iList As New List(Of AppInfo)
+        For Each app As String In Registry.LocalMachine.OpenSubKey(DestKey).GetSubKeyNames
+            iList.Add(New AppInfo With {.Name = Registry.LocalMachine.OpenSubKey(DestKey & app & "\").GetValue("DisplayName"), _
+                        .UnInstallPath = Registry.LocalMachine.OpenSubKey(DestKey & app & "\").GetValue("UninstallString")})
+        Next
+        Return iList
+    End Function
+
+    Private Sub ExportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportToolStripMenuItem.Click
+        Dim xlApp As Interop.Excel.Application
+        Dim xlWorkBook As Interop.Excel.Workbook
+        Dim xlWorkSheet As Interop.Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+        Dim i As Integer
+        Dim j As Integer
+
+        xlApp = New Microsoft.Office.Interop.Excel.Application
+        xlWorkBook = xlApp.Workbooks.Add(misValue)
+        xlWorkSheet = xlWorkBook.Sheets("sheet1")
+
+
+        For i = 0 To DataGridView1.RowCount - 2
+            For j = 0 To DataGridView1.ColumnCount - 1
+                For k As Integer = 1 To DataGridView1.Columns.Count
+                    xlWorkSheet.Cells(1, k) = DataGridView1.Columns(k - 1).HeaderText
+                    xlWorkSheet.Cells(i + 2, j + 1) = DataGridView1(j, i).Value.ToString()
+                Next
+            Next
+        Next
+
+        xlWorkSheet.SaveAs("D:\" & Environment.UserName & "vbexcel.xlsx")
+        xlWorkBook.Close()
+        xlApp.Quit()
+
+        releaseObject(xlApp)
+        releaseObject(xlWorkBook)
+        releaseObject(xlWorkSheet)
+
+        MsgBox("You can find the file D:\vbexcel.xlsx")
+    End Sub
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+End Class
